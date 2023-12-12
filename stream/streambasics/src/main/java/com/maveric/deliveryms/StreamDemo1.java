64d16b259ff53629998568e44d34cc2aa041aa71
vineetsemwal <vineetsemwal82@gmail.com>
2023-12-12 17:41:53 +0530

    minor formatting change

--- stream/streambasics/src/main/java/com/maveric/deliveryms/StreamDemo1.java
+++ stream/streambasics/src/main/java/com/maveric/deliveryms/StreamDemo1.java
@@ -15,44 +15,47 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
+
 // Serde Serializer-Deserializer
 public class StreamDemo1 {
-    private static final Logger Log= LoggerFactory.getLogger(StreamDemo1.class);
+    private static final Logger Log = LoggerFactory.getLogger(StreamDemo1.class);
+
     public static void main(String[] args) throws Exception {
 
-        Properties properties=KafkaPropertiesReader.read("application.properties");
-        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,Serdes.String().getClass());
-        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.String().getClass());
+        Properties properties = KafkaPropertiesReader.read("application.properties");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-        Log.info("****properties"+properties);
-        StreamsBuilder builder=new StreamsBuilder();
+        Log.info("****properties" + properties);
+        StreamsBuilder builder = new StreamsBuilder();
         //source processor giving us stream from topic input-words
-        KStream<String,String>inputStream= builder.stream("input-words", Consumed.with(Serdes.String(),Serdes.String()));
+        KStream<String, String> inputStream = builder.stream("input-words", Consumed.with(Serdes.String(), Serdes.String()));
 
-          KStream<String,String>upperStream    = inputStream .peek((key,value)-> Log.info("key="+key+"value="+value))
-          //intermediate map processor giving us new KStream<String,String> where key is string, value is message in uppercase
-                .mapValues((value)->value.toUpperCase())
-                .peek((key,value)-> System.out.println("key="+key+",value="+value));
-
-          //intermediate map processor giving us new KStream<String,Integer> where key is string, value is message length
-        inputStream.mapValues(value -> value.length())
-                .peek((key,value)-> System.out.println("key="+key+",value="+value))
-           //sink processor sinking to topic words-length
-                .to("words-length",Produced.with(Serdes.String(),Serdes.Integer()));
-
+        KStream<String, String> upperStream = inputStream.peek((key, value) -> Log.info("key=" + key + "value=" + value))
+                //intermediate map processor giving us new KStream<String,String> where key is string, value is message in uppercase
+                .mapValues((value) -> value.toUpperCase())
+                .peek((key, value) -> System.out.println("key=" + key + ",value=" + value));
         //sink processor sinking to topics upper-words
-                upperStream.to("upper-words", Produced.with(Serdes.String(), Serdes.String()));
-       Topology topology= builder.build();
-       Log.info("*****topology="+topology.describe());
+        upperStream.to("upper-words", Produced.with(Serdes.String(), Serdes.String()));
 
-        try(KafkaStreams streams=new KafkaStreams(topology,properties)) {
-                streams.start();
-                Thread.sleep(10000000);
+        //intermediate map processor giving us new KStream<String,Integer> where key is string, value is message length
+        inputStream.mapValues(value -> value.length())
+                .peek((key, value) -> System.out.println("key=" + key + ",value=" + value))
+                //sink processor sinking to topic words-length
+                .to("words-length", Produced.with(Serdes.String(), Serdes.Integer()));
+
+
+        Topology topology = builder.build();
+        Log.info("*****topology=" + topology.describe());
+
+        try (KafkaStreams streams = new KafkaStreams(topology, properties)) {
+            streams.start();
+            Thread.sleep(10000000);
 
         }
 
 
-        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             Log.info("*** application closed");
         }));
 
